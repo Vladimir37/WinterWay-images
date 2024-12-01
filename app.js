47fc9471cc5e6a98c21bb0561bae9ae4c7520370
Vladimir37 <vladimir37work@gmail.com>
2024-12-01 22:10:13 +0300

    Name validation and empty background

--- app.js
+++ app.js
@@ -14,17 +14,39 @@ let responseObj = {
 function countImageFiles(dirPath) {
     try {
         const files = fs.readdirSync(dirPath);
-        return files.filter(file => path.extname(file).toLowerCase() === `.${config.extension}`).length;
+        return files.length;
     } catch (err) {
-        console.log(`Error reading directory ${dirPath}:`, err);
+        throw new Error(`ERROR: Error reading directory ${dirPath}: ${err.message}`);
+    }
+}
+
+function validateFileNaming(dir) {
+    const files = fs.readdirSync(dir)
+        .filter(file => path.extname(file).toLowerCase() === `.${config.extension}`)
+        .map(file => path.basename(file, `.${config.extension}`))
+        .sort((a, b) => Number(a) - Number(b));
+
+    for (let i = 0; i < files.length; i++) {
+        if (Number(files[i]) !== i + 1) {
+            throw new Error(`ERROR: The naming of ${dir}/${files[i]}.${config.extension} is inconsistent with the rest. File numbers must follow a sequential order and start from 1.`);
+        }
+    }
+
+    const incorrectFileExtensions = fs.readdirSync(dir)
+        .filter(file => path.extname(file).toLowerCase() !== `.${config.extension}`);
+
+    if (incorrectFileExtensions.length > 0) {
+        throw new Error(`ERROR: Files with incorrect extensions detected. All files must have an extension specified in config.json, in this case, ".${config.extension}". Invalid files: ${incorrectFileExtensions.join()}`);
     }
 }
 
 function calculateFileCounts() {
-    const dirs = ['backlog', 'days', 'months', 'none', 'other'];
+    const dirs = ['Backlog', 'Days', 'Months', 'None', 'Other', 'Empty'];
 
     dirs.forEach(dir => {
-        responseObj.Count[dir] = countImageFiles(`./${config.imageDir}/${dir}`);
+        const fullDirPath = `./${config.imageDir}/${dir}`;
+        validateFileNaming(fullDirPath);
+        responseObj.Count[dir] = countImageFiles(fullDirPath);
     });
 }
 
